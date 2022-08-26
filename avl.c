c7ea50868207267baa39205bac98c2c44f2c8a84
Adwaith R Krishna <adwaithrk19@gmail.com>
2022-08-26 22:43:35 +0530

    deletion

--- avl.c
+++ avl.c
@@ -104,3 +104,88 @@ struct avlNode *insert_node_into_avl(int value, struct avlNode *root)
 
     return root;
 }
+
+struct avlNode *find_inorder_successor(struct avlNode *current)
+{
+    if (current->right != NULL)
+    {
+        current = current->right;
+
+        while (current->left != NULL)
+        {
+            current = current->left;
+        }
+
+        return current;
+    }
+    return NULL;
+}
+
+struct avlNode *delete_avl_node(int value, struct avlNode *root)
+{
+    if (root == NULL)
+        return root;
+
+    if (root->value > value)
+        root = delete_avl_node(value, root->left);
+
+    else if (root->value < value)
+        root = delete_avl_node(value, root->right);
+    else
+    {
+        if (root->left == NULL || root->right == NULL)
+        {
+            struct avlNode *temp = root->left ? root->left : root->right;
+
+            if (temp == NULL)
+            {
+                root = temp;
+            }
+            else
+            {
+                *root = *temp;
+            }
+            free(temp);
+        }
+        else
+        {
+            struct avlNode *temp = find_inorder_successor(root);
+            root->value = temp->value;
+
+            root->right = delete_avl_node(temp->value, root->right);
+        }
+    }
+
+    if (root == NULL)
+        return root;
+
+    root->height = 1 + max(height(root->left),
+                           height(root->right));
+
+    int balance = getBalance(root);
+
+    if (balance > 1 && getBalance(root->left) >= 0)
+        return right_rotate(root);
+
+    // Left Right Case
+    if (balance > 1 && getBalance(root->left) < 0)
+    {
+        root->left = left_rotate(root->left);
+        return right_rotate(root);
+    }
+
+    // Right Right Case
+    if (balance < -1 && getBalance(root->right) <= 0)
+        return left_rotate(root);
+
+    // Right Left Case
+    if (balance < -1 && getBalance(root->right) > 0)
+    {
+        root->right = right_rotate(root->right);
+        return left_rotate(root);
+    }
+
+    return root;
+}
+
+int main() {}
